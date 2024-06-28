eb7cc176ad5d0ed84474ff4091e30f72daeaffd4
pavel.donchev@eveliko.com <pavel.donchev@eveliko.com>
2024-06-28 14:58:17 +0300

    - Changes to the PDF to quite and also receive url as a parameter to print.

--- PDF_Converter_Prototype/Controllers/PDFController.cs
+++ PDF_Converter_Prototype/Controllers/PDFController.cs
@@ -7,11 +7,16 @@ namespace PDF_Converter_Prototype.Controllers
     [Route("[controller]")]
     public class PDFController : ControllerBase
     {
-        HTMLtoPDFConverter converter = new();
+        public IHttpContextAccessor _accessor = null;
+        public PDFController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
 
         [HttpGet(Name = "GET-PDF")]
         public FileContentResult Get_PDF()
         {
+            var converter = new HTMLtoPDFConverter(_accessor);
             var bytes = converter.PrintHTMLToPDF();
             return File(bytes, "application/pdf", "PDF_Example.pdf");
         }
--- PDF_Converter_Prototype/Converters/HTMLtoPDFConverter.cs
+++ PDF_Converter_Prototype/Converters/HTMLtoPDFConverter.cs
@@ -8,32 +8,57 @@ namespace PDF_Converter_Prototype.Converters
         public WebDriver _driver;
         public static string fileName = "PDF_Example";
         public static string printFinalPath = System.IO.Directory.GetCurrentDirectory() + @$"\{fileName}";
-        
+
+        public IHttpContextAccessor _contextAccessor = null;
+
+        public HTMLtoPDFConverter(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
         public byte[] PrintHTMLToPDF()
         {
-            _chromeOptions = new ChromeOptions();
-            _chromeOptions.AddArguments("--no-sandbox");
-            _chromeOptions.AddArgument("--disable-dev-shm-usage");
-            _chromeOptions.AddArgument("--whitelisted-ips=");
-            _chromeOptions.AddArguments("--headless");
-
-            //creating webdriver
-            _driver = new ChromeDriver(_chromeOptions);
-
-            //go to the website
-            _driver.Navigate().GoToUrl("https://www.eveliko.com/");
-
-            //defining configurations for de printing
-            PrintOptions printOptions = new PrintOptions
+            try
             {
-                Orientation = PrintOrientation.Portrait
-            };
+                string url = "https://www.eveliko.com";
 
-            //printing...
-            PrintDocument printDocument = _driver.Print(printOptions);
+                if (!string.IsNullOrWhiteSpace(_contextAccessor.HttpContext.Request.Query["printUrl"]))
+                {
+                    url = _contextAccessor.HttpContext.Request.Query["printUrl"];
+                }
 
-            return printDocument.AsByteArray;
+                _chromeOptions = new ChromeOptions();
+                _chromeOptions.AddArguments("--no-sandbox");
+                _chromeOptions.AddArgument("--disable-dev-shm-usage");
+                _chromeOptions.AddArgument("--whitelisted-ips=");
+                _chromeOptions.AddArguments("--headless");
 
+                //creating webdriver
+                _driver = new ChromeDriver(_chromeOptions);
+
+                //go to the website
+                _driver.Navigate().GoToUrl(url);
+
+                //defining configurations for de printing
+                PrintOptions printOptions = new PrintOptions
+                {
+                    Orientation = PrintOrientation.Portrait
+                };
+
+                //printing...
+                PrintDocument printDocument = _driver.Print(printOptions);
+                try
+                {
+                    _driver.Quit();
+                }
+                catch { } // On Purpose, we want to ensure Quit doesn't interupt already rendered doc.
+                return printDocument.AsByteArray;
+            }
+            catch
+            {
+                _driver.Quit();
+                return null;
+            }
             //saving the file
             //printDocument.SaveAsFile(printFinalPath);
             //return File.ReadAllBytes(printFinalPath);
